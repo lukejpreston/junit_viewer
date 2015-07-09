52173058f4655ed0eb152abf2a91a49a3cc8cf05
Laszlo Szikszai <Laszlo_Szikszai@epam.com>
2015-07-09 13:49:24 +0200

    - Added SKIP step type in UI  - Added semicolons

--- index.js
+++ index.js
@@ -1,131 +1,131 @@
 var path = require('path'),
     fs = require('fs'),
-    parser = new require('xml2js').Parser()
+    parser = new require('xml2js').Parser();
 
 
-var commandArgs = {}
+var commandArgs = {};
 
 process.argv.forEach(function(arg) {
     if (arg.indexOf('--save') !== -1) {
-        commandArgs.save = arg.split('=')[1] || 'junit.html'
+        commandArgs.save = arg.split('=')[1] || 'junit.html';
     }
     if (arg.indexOf('--folder') !== -1) {
-        commandArgs.folder = arg.split('=')[1] || './test_data'
+        commandArgs.folder = arg.split('=')[1] || './test_data';
     }
     if (arg.indexOf('--port') !== -1) {
-        commandArgs.port = arg.split('=')[1] || 9090
+        commandArgs.port = arg.split('=')[1] || 9090;
     }
-})
+});
 
-var folder = commandArgs.folder || './test_data'
+var folder = commandArgs.folder || './test_data';
 
 if (folder.charAt(0) == '.' && folder.charAt(1) == '/') {
-    folder = folder.replace('./', '')
+    folder = folder.replace('./', '');
 }
 
-console.log(folder)
+console.log(folder);
 
 if (folder.charAt(folder.length - 1) !== '/') {
-    folder += '/'
+    folder += '/';
 }
-
-
 
 function endsWith(str, suffix) {
     return str.indexOf(suffix, str.length - suffix.length) !== -1;
 }
 
 function getFiles(folder) {
-    var files = []
+    var files = [];
     fs.readdirSync(folder).forEach(function(file) {
         if (fs.lstatSync(folder + '/' + file).isDirectory()) {
-            files = files.concat(getFiles(folder + '/' + file))
+            files = files.concat(getFiles(folder + '/' + file));
         } else {
-            files.push(folder + '/' + file)
+            files.push(folder + '/' + file);
         }
-    })
+    });
 
     return files.filter(function(file) {
-        var suffix = '.xml'
+        var suffix = '.xml';
         return file.indexOf(suffix, file.length - suffix.length) !== -1
-    })
+    });
 }
 
 function jsonResults() {
-    var files = getFiles(folder)
+    var files = getFiles(folder);
 
-    var finalResults = {}
+    var finalResults = {};
     var testFiles = files.map(function(file) {
-        var data = fs.readFileSync(file)
-        var parsedData
+        var data = fs.readFileSync(file);
+        var parsedData;
         parser.parseString(data, function(err, result) {
-            parsedData = result || {}
-        })
-        var fileSplit = file.split('/')
-        parsedData.name = fileSplit[fileSplit.length - 1].split('.')[0]
-        return parsedData
+            parsedData = result || {};
+        });
+        var fileSplit = file.split('/');
+        parsedData.name = fileSplit[fileSplit.length - 1].split('.')[0];
+        return parsedData;
     }).filter(function(result) {
-        return result.hasOwnProperty('testsuite') || result.hasOwnProperty('testsuites')
-    })
+        return result.hasOwnProperty('testsuite') || result.hasOwnProperty('testsuites');
+    });
 
-    var results = []
+    var results = [];
     testFiles.forEach(function(file) {
         if (file.hasOwnProperty('testsuites')) {
             file.testsuites.testsuite.forEach(function(test) {
-                results = results.concat(test)
-            })
+                results = results.concat(test);
+            });
         } else {
-            results.push(file.testsuite)
+            results.push(file.testsuite);
         }
-    })
+    });
 
     results.filter(function(result) {
-        return result.testcase !== undefined
+        return result.testcase !== undefined;
     }).forEach(function(result) {
-        var testCases = result.testcase
+        var testCases = result.testcase;
         testCases.forEach(function(testCase) {
-            var name = testCase.$.classname
+            var name = testCase.$.classname;
             if (!finalResults.hasOwnProperty(name)) {
                 finalResults[name] = {
                     tests: 0,
                     failures: 0,
                     cases: []
-                }
+                };
             }
 
-            finalResults[name].tests += 1
+            finalResults[name].tests += 1;
 
             var newCase = {
                 name: testCase.$.name.replace(new RegExp('\n', 'g'), '<br />'),
                 time: testCase.$.time
-            }
+            };
 
             if (testCase.hasOwnProperty('failure')) {
-                finalResults[name].failures += 1
-                newCase.failureMessage = testCase.failure[0]._
+                finalResults[name].failures += 1;
+                newCase.failureMessage = testCase.failure[0]._;
+            } else if (testCase.hasOwnProperty('skipped')) {
+                newCase.skipped = true;
             }
-            finalResults[name].cases.push(newCase)
-        })
-    })
-    return finalResults
+            finalResults[name].cases.push(newCase);
+        });
+    });
+    return finalResults;
 }
 
-var express = require('express')
+var express = require('express');
 
 function getTitle() {
-    var folderSplit = folder.split('/')
-    var title = folderSplit[folderSplit.length - 2]
-    title = title.replace(new RegExp('_', 'g'), ' ')
-    var fullTitle = []
+    var folderSplit = folder.split('/');
+    var title = folderSplit[folderSplit.length - 2];
+    title = title.replace(new RegExp('_', 'g'), ' ');
+    var fullTitle = [];
     title.split(' ').forEach(function(word) {
-        fullTitle.push(word[0].toUpperCase() + word.slice(1))
-    })
-    return fullTitle.join(' ')
+        fullTitle.push(word[0].toUpperCase() + word.slice(1));
+    });
+    return fullTitle.join(' ');
 }
 
 function startServer() {
-    app = express()
-    app.use(express.static(__dirname))
+    var app = express();
+    app.use(express.static(__dirname));
 
     app.get('/junit.json', function(req, res) {
         res.send({
@@ -139,13 +139,13 @@ function startServer() {
 }
 
 function save() {
-    var thing = fs.readFileSync(__dirname + '/template.html')
-    var thing = thing
+    var thing = fs.readFileSync(__dirname + '/template.html');
+    thing = thing
         .toString()
         .replace('REPLACEWITHJUNITDATA', JSON.stringify({
             results: jsonResults(),
             title: getTitle()
-        }))
+        }));
     fs.writeFile(commandArgs.save, thing, function(err) {
         if (err) {
             return console.log(err);
@@ -161,18 +161,18 @@ function start() {
             '--folder=folderName location of folder',
             '--port=portNumber supply a port if you want to serve',
             '--save=fileName supply a file name if you wish to save the file'
-        ].join('\n')
-        console.log(message)
+        ].join('\n');
+        console.log(message);
     } else {
         if (commandArgs.port) {
-            startServer()
+            startServer();
         }
         if (commandArgs.save) {
-            save()
+            save();
         }
     }
 }
 
 module.exports = {
     start: start
-}
+};
--- junit_parser.js
+++ junit_parser.js
@@ -1,143 +1,140 @@
 function Dom(element) {
-    return $('<' + element + '></' + element + '>')
+    return $('<' + element + '></' + element + '>');
 }
 
 function Div() {
-    return Dom('div')
+    return Dom('div');
 }
 
 function Span() {
-    return Dom('span')
+    return Dom('span');
 }
 
 function Br() {
-    return Dom('br')
+    return Dom('br');
 }
 
-$('.error').hide()
+$('.error').hide();
 
-$.getJSON("junit.json", function(junitData) {
-    $(".title").text(junitData.title)
-    $('title').text(junitData.title)
-    delete junitData.title
+$.getJSON("junit.json", function (junitData) {
+    $(".title").text(junitData.title);
+    $('title').text(junitData.title);
+    delete junitData.title;
 
-    var index = 0
+    var index = 0;
     for (var className in junitData.results) {
-        var data = junitData.results[className]
-        var suite = Div()
-            .attr('id', function() {
-                return 's_' + index
-            })
-            .append(Div()
-                .append(
-                    Span()
-                    .html(className)
-                    .addClass('suite__name')
-                )
-                .append(
-                    Span()
-                    .html('Tests: ' + data.tests)
-                    .addClass('suite__data')
-                )
-                .append(
-                    Span()
-                    .html('Failures: ' + data.failures)
-                    .addClass('suite__data')
-                )
-                .addClass('suite')
-            )
-            .addClass("suite--contracted")
-            .append()
-            .appendTo($('#results'))
-
-        if (data.failures === 0)
-            suite.addClass('suite--pass')
-        else
-            suite.addClass('suite--fail')
-
-        var tests = Div()
-            .addClass('tests')
-
-        data.cases.forEach(function(testCase, caseIndex) {
-            var test = Div()
-                .attr('id', function() {
-                    return 't_' + caseIndex
+        if (junitData.results.hasOwnProperty(className)) {
+            var data = junitData.results[className];
+            var suite = Div()
+                .attr('id', function () {
+                    return 's_' + index;
                 })
-                .append(
-                    Span()
-                    .html(testCase.name)
-                    .addClass('test__name')
-                )
-                .append(
-                    Span()
-                    .html('Time: ' + parseInt(testCase.time).toFixed(2))
-                    .addClass('test__time')
-                )
-                .addClass('test')
+                .append(Div()
+                    .append(
+                        Span()
+                            .html(className)
+                            .addClass('suite__name')
+                    ).append(
+                        Span()
+                            .html('Tests: ' + data.tests)
+                            .addClass('suite__data')
+                    ).append(
+                        Span()
+                            .html('Failures: ' + data.failures)
+                            .addClass('suite__data')
+                    ).addClass('suite')
+                ).addClass("suite--contracted")
+                .append()
+                .appendTo($('#results'));
 
-            if (testCase.hasOwnProperty('failureMessage'))
-                test.append(
-                    Br()
-                ).append(
-                    Span()
-                    .html('Message:')
-                ).append(
-                    Br()
-                ).append(
-                    Span()
-                    .html(testCase.failureMessage)
-                    .addClass('test__fail')
-                )
-                .addClass('test--fail')
-            else
-                test.addClass('test--pass')
+            if (data.failures === 0) {
+                suite.addClass('suite--pass');
+            } else {
+                suite.addClass('suite--fail');
+            }
 
-            tests
-                .hide()
-                .append(test)
-        })
-        suite
-            .append(tests)
-            .on('click', function() {
-                var self = $(this)
-                self
-                    .children('.tests')
-                    .slideToggle(500)
+            var tests = Div().addClass('tests');
 
-                if (self.hasClass("suite--contracted")) {
-                    self.removeClass("suite--contracted")
-                    self.addClass("suite--expanded")
+            data.cases.forEach(function (testCase, caseIndex) {
+                var test = Div()
+                    .attr('id', function () {
+                        return 't_' + caseIndex;
+                    })
+                    .append(
+                        Span()
+                            .html(testCase.name)
+                            .addClass('test__name')
+                    ).append(
+                        Span()
+                            .html('Time: ' + parseInt(testCase.time).toFixed(2))
+                            .addClass('test__time')
+                    ).addClass('test');
+
+                if (testCase.hasOwnProperty('failureMessage')) {
+                    test.append(
+                        Br()
+                    ).append(
+                        Span()
+                            .html('Message:')
+                    ).append(
+                        Br()
+                    ).append(
+                        Span()
+                            .html(testCase.failureMessage)
+                            .addClass('test__fail')
+                    ).addClass('test--fail');
+                } else if (testCase.hasOwnProperty('skipped')) {
+                    test.addClass('test--skip');
                 } else {
-                    self.addClass("suite--contracted")
-                    self.removeClass("suite--expanded")
+                    test.addClass('test--pass');
                 }
-            })
-        index += 1
+
+                tests
+                    .hide()
+                    .append(test);
+            });
+            suite
+                .append(tests)
+                .on('click', function () {
+                    var self = $(this);
+                    self
+                        .children('.tests')
+                        .slideToggle(500);
+
+                    if (self.hasClass("suite--contracted")) {
+                        self.removeClass("suite--contracted");
+                        self.addClass("suite--expanded");
+                    } else {
+                        self.addClass("suite--contracted");
+                        self.removeClass("suite--expanded");
+                    }
+                });
+            index += 1;
+        }
     }
     if (index === 0 || junitData.results.hasOwnProperty("NO FOLDER SPECIFIED FOR JUNIT VIEWER")) {
-        $('.error').show()
+        $('.error').show();
     }
-})
+});
 
 function filter() {
-    $(".test--pass").slideToggle(500)
-    $(".suite--pass").slideToggle(500)
-    var text = $(".filter").text()
-    if (text.indexOf("Filter") !== -1)
-        $(".filter").text("Insert Passing")
-    else
-        $(".filter").text("Filter Passing")
+    $(".test--pass").slideToggle(500);
+    $(".suite--pass").slideToggle(500);
+    var text = $(".filter").text();
+    if (text.indexOf("Filter") !== -1) {
+        $(".filter").text("Insert Passing");
+    } else {
+        $(".filter").text("Filter Passing");
+    }
 }
 
 function expand() {
-    
-    var text = $(".expand").text()
+    var text = $(".expand").text();
     if (text.indexOf("Expand") !== -1) {
-        $(".expand").text("Contract All")
-        $(".suite--contracted").click()
-    }
-    else {
-        $(".suite--expanded").click()
-        $(".expand").text("Expand All")
+        $(".expand").text("Contract All");
+        $(".suite--contracted").click();
+    } else {
+        $(".suite--expanded").click();
+        $(".expand").text("Expand All");
     }
 }
