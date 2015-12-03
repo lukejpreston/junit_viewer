bde7ae7a45fc2e092652ee92ca5c64203d104550
Luke Preston <luke.preston@bbc.co.uk>
2015-12-03 09:12:44 +0000

    have tests for passing data

--- __tests__/parser-test.js
+++ __tests__/parser-test.js
@@ -32,4 +32,28 @@ describe('Parsing junit results', function() {
             })
         })
     })
+
+    describe('Failing suite', function() {
+        var suite = testData.suites.Failing
+
+        it('Has a name', function() {
+            expect(suite.name).toBeDefined()
+        })
+
+        it('Has the time', function() {
+            expect(suite.time).toBeDefined()
+        })
+
+        describe('Each test', function() {
+            suite.tests.forEach(function(test) {
+                it('Has a name', function() {
+                    expect(test.name).toBeDefined()
+                })
+
+                it('Has type of "passed"', function() {
+                    expect(test.type).toBe('passed')
+                })
+            })
+        })
+    })
 })
