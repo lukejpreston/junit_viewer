21fa3c27d4d78c275ab8159e6c1b6851719e6876
Luke Preston <luke.preston@bbc.co.uk>
2015-12-03 17:43:58 +0000

    adding parser tests and coverage tool

--- __tests__/parser-test.js
+++ __tests__/parser-test.js
@@ -3,7 +3,8 @@ jest.autoMockOff()
 var parser = require('../parser')
 
 describe('Parsing junit results', function() {
-    var testData = parser.parse(__dirname + '/data')
+    var testData = parser.parse(__dirname + '/data/')
+    var singleTestData = parser.parse(__dirname + '/data/passing_suite.xml')
 
     it('Has the title of the file', function() {
         expect(testData.title).toBe('Data')
@@ -50,4 +51,38 @@ describe('Parsing junit results', function() {
         name: 'Skipped',
         type: 'skipped'
     }].forEach(hasBasicSchema)
+
+    describe('Failing test', function() {
+        it('Has a message', function() {
+            testData.suites.Failing.tests.forEach(function(test) {
+                expect(test.message).toBeDefined()
+            })
+        })
+    })
+
+    describe('Error test', function() {
+        it('Has a message', function() {
+            testData.suites.Error.tests.forEach(function(test) {
+                expect(test.message).toBeDefined()
+            })
+        })
+    })
+
+    describe('Properties', function() {
+        it('Suites can have properties', function() {
+            expect(testData.suites.Defect.properties).toBeDefined()
+        })
+    })
+
+    it('Can have multiple tests', function() {
+        expect(Array.isArray(testData.suites.Multi.tests)).toBe(true)
+    })
+
+    it('Adds tests in sub folders', function() {
+        expect(testData.suites['Multi Sub']).toBeDefined()
+    })
+
+    it('Can parse just a single file', function() {
+        expect(singleTestData).toBeDefined()
+    })
 })
