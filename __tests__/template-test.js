a7dc316a26822b97978e6cc3ba5f1e2cb0a83e12
Luke Preston <luke.preston@bbc.co.uk>
2015-12-04 07:50:03 +0000

    renders string to dom to make it easier to test

--- __tests__/template-test.js
+++ __tests__/template-test.js
@@ -3,10 +3,14 @@ jest.autoMockOff()
 var data = require('./data/output.json'),
     template = require('../template')
 
-var thingy = require('jsdom').jsdom(template.render(data))
+var rednerdView = require('jsdom').jsdom(template.render(data))
 
 describe('Template', function() {
     it('Replace the title with the test name', function() {
-        expect(thingy.title).toBe(data.title)
+        expect(rednerdView.title).toBe(data.title)
     });
+
+    it('Has the head with the test name', function() {
+        expect(rednerdView.getElementByTagName('h1')[0].innerHTML).toBe(data.title)
+    })
 });
