f713ad4debc8e1dd1ad0ea929c9cffe8eded5135
Luke Preston <luke.preston@bbc.co.uk>
2015-12-10 10:04:31 +0000

    better styling

--- templates/junit_viewer.js
+++ templates/junit_viewer.js
@@ -1,38 +1,5 @@
 var suites = {{{suites}}}
 
-var names = Object.keys(suites).map(function(key) {
-    var tests = suites[key].tests.map(function(test) {
-        return {
-            name: test.name.toLowerCase(),
-            id: test.id
-        }
-    })
-    return {
-        suite: {
-            name: suites[key].name.toLowerCase(),
-            id: suites[key].id
-        },
-        tests: tests
-    }
-})
-
 function search(value) {
-    if (value === '') {
-        names.forEach(function(name) {
-            document.getElementById(name.suite.id).style.display = 'block'
-        })
-    } else {
-        value = value.toLowerCase()
-        names.filter(function(name) {
-            return name.suite.name.indexOf(value) === -1
-        }).forEach(function(name) {
-            document.getElementById(name.suite.id).style.display = 'none'
-        })
-
-        names.filter(function(name) {
-            return name.suite.name.indexOf(value) !== -1
-        }).forEach(function(name) {
-            document.getElementById(name.suite.id).style.display = 'block'
-        })
-    }
+    
 }
