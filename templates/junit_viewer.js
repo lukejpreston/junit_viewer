87a451175d2d00dd63130fbf698f21410ed22873
Luke Preston <luke.preston@bbc.co.uk>
2015-12-10 10:21:50 +0000

    clicking on suites, tests and properties contracts them

--- templates/junit_viewer.js
+++ templates/junit_viewer.js
@@ -1,5 +1,9 @@
-var suites = {{{suites}}}
+// var suites = {{{suites}}}
 
-function search(value) {
-    
+function contract(element) {
+    if (element.children[1]) {
+        var visible = element.children[1].style.display
+        element.children[1].style.display = visible !== 'none' ? 'none' : 'block'
+        element.children[0].style['border-radius'] = visible !== 'none' ? '4px' : '4px 4px 0 0'
+    }
 }
