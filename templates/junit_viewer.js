ee53971f145d19136bf7fa562ccd102413619f14
Luke Preston <luke.preston@bbc.co.uk>
2015-12-10 10:39:32 +0000

    can hide passing suites

--- templates/junit_viewer.js
+++ templates/junit_viewer.js
@@ -1,4 +1,4 @@
-// var suites = {{{suites}}}
+var suites = {{{suites}}}
 
 function contract(element) {
     if (element.children[1]) {
@@ -6,4 +6,24 @@ function contract(element) {
         element.children[1].style.display = visible !== 'none' ? 'none' : 'block'
         element.children[0].style['border-radius'] = visible !== 'none' ? '4px' : '4px 4px 0 0'
     }
+}
+
+function forEachSuite(callback) {
+    Object.keys(suites).forEach(function(key) {
+        callback(suites[key])
+    })
+}
+
+function hideSuites(element) {
+    var shown = element.innerHTML.indexOf('HIDE') !== -1
+    element.innerHTML = (shown ? 'SHOW' : 'HIDE') + ' PASSING SUITES'
+
+    forEachSuite(function(suite) {
+        if(suite.type === 'passed')
+            document.getElementById(suite.id).style.display = shown ? 'none' : 'block'
+    })
+}
+
+function hideTests(element) {
+
 }
