b993515ca1fe64d732217bfa3829839ec830e023
Luke Preston <luke.preston@bbc.co.uk>
2015-12-14 19:32:11 +0000

    not sure reverting I think

--- render.js
+++ render.js
@@ -42,11 +42,13 @@ module.exports = function(data) {
         if (suite.testCases) {
             suite.rendered.tests = suite.testCases.map(function(test) {
                 test.rendered = {}
-                if (test.messages) {
+                if (test.messages && test.messages.length > 1) {
                     test.rendered.messages = test.messages.map(function(message) {
-                            return render('test_message.html', message)
-                        }).join('\n')
+                        return render('test_message.html', message)
+                    }).join('\n')
                     test.rendered.testMessages = render('test_messages.html', test)
+                } else if (test.messages) {
+                    test.rendered.testMessages = render('test_message.html', test.messages[0])
                 }
                 return render('test.html', test)
             }).join('\n')
