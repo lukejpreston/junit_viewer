0ecaf96d7f58d25e9fd032109f7195e9485ab8e7
Luke Preston <luke.preston@bbc.co.uk>
2015-12-16 10:04:42 +0000

    adding even more test data for parsing

--- generate_view.js
+++ generate_view.js
@@ -6,10 +6,16 @@ var validData = parse(__dirname + '/__tests__/data')
 var redneredValidData = render(validData)
 fs.writeFileSync('valid_data.html', redneredValidData)
 
-var singleFile = parse(__dirname + '/__tests__/data/multi_suite.xml')
-var renderedSingleFile = render(singleFile)
-fs.writeFileSync('single_file.html', renderedSingleFile)
+// var singleFile = parse(__dirname + '/__tests__/data/multi_suite.xml')
+// var renderedSingleFile = render(singleFile)
+// fs.writeFileSync('single_file.html', renderedSingleFile)
 
-var noFile = parse(__dirname + '/__tests__/dat')
-var renderedNoFile = render(noFile)
-fs.writeFileSync('no_file.html', renderedNoFile)
+// var noFile = parse(__dirname + '/__tests__/dat')
+// var renderedNoFile = render(noFile)
+// fs.writeFileSync('no_file.html', renderedNoFile)
+
+
+// var manyData = parse(__dirname + '/__tests__/data/multi_suite.xml')
+// var manyTests = parse(manyData)
+// var redneredManyTests = render(manyTests)
+// fs.writeFileSync('many_tests.html', redneredManyTests)
