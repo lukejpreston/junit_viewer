41a567db3b12985663564b3f006623b4c7f42922
Luke Preston <luke.preston@bbc.co.uk>
2015-12-17 07:06:16 +0000

    adding other views back in

--- generate_view.js
+++ generate_view.js
@@ -7,13 +7,13 @@ fs.writeFileSync('valid_data.json', JSON.stringify(validData))
 var redneredValidData = render(validData)
 fs.writeFileSync('valid_data.html', redneredValidData)
 
-// var singleFile = parse(__dirname + '/__tests__/data/multi_suite.xml')
-// var renderedSingleFile = render(singleFile)
-// fs.writeFileSync('single_file.html', renderedSingleFile)
+var singleFile = parse(__dirname + '/__tests__/data/multi_suite.xml')
+var renderedSingleFile = render(singleFile)
+fs.writeFileSync('single_file.html', renderedSingleFile)
 
-// var noFile = parse(__dirname + '/__tests__/dat')
-// var renderedNoFile = render(noFile)
-// fs.writeFileSync('no_file.html', renderedNoFile)
+var noFile = parse(__dirname + '/__tests__/dat')
+var renderedNoFile = render(noFile)
+fs.writeFileSync('no_file.html', renderedNoFile)
 
 
 // var manyData = parse(__dirname + '/__tests__/data/multi_suite.xml')
