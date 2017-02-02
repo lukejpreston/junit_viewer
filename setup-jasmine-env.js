c1d9fcda733e0bc86c4c5b9a671b116cc326cc7e
Luke Preston <luke.preston@bbc.co.uk>
2017-02-02 12:28:32 +0000

    clean react slate

--- setup-jasmine-env.js
+++
@@ -1,5 +0,0 @@
-jasmine.VERBOSE = true
-
-require('jasmine-reporters')
-var reporter = new jasmine.JUnitXmlReporter('junit_viewer_specs')
-jasmine.getEnv().addReporter(reporter)
--- src/main.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    parse: require('./parse').parse,
-    parseXML: require('./parse').parseXML,
-    render: require('./render'),
-    junit_viewer: require('./junit_viewer')
-}
